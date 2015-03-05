f1c1aeaa8a0cb0a8d6d6742d1ad3e5def45cda43
William E. Byrd <webyrd@gmail.com>
2015-03-05 03:19:31 -0700

    adding unspecified behavior examples

--- unspecified-behavior/interp-curried-two-directions-tests.scm
+++ unspecified-behavior/interp-curried-two-directions-tests.scm
@@ -5,13 +5,32 @@
 ;;
 ;; The 'eval-left-to-righto' relation evaluates arguments in
 ;; left-to-right order, while the 'eval-right-to-lefto' relation
-;; evaluates arguments in right-to-left order.
+;; evaluates arguments in right-to-left order.  For example, when
+;; evaluating the expression (cons e1 e2), where e1 and e2 are
+;; sub-expressions, 'eval-left-to-righto' will evaluate e1 before
+;; evaluating e2, while 'eval-right-to-lefto' will use the opposite
+;; evaluation order.
+;;
+;; This difference in evaluation order is only visible when an
+;; expression contains an effectful expression in argument position.
+;; For example, the value of the variable x after evaluating
+;; (cons (set! x 5) (set! x 6)) depends on which set! is evaluated
+;; second.  The Scheme specification does not specify the order in
+;; which expressions in argument position should be evaluated, so
+;; the behavior of this example is implementation specific.
+;; Vicare Scheme, Petite Chez Scheme, and Racket differ in this
+;; regard: Racket evaluates left-to-right, Petite seems to evaluate
+;; right-to-left, and Vicare seems to use both left-to-right and
+;; right-to-left evaluation, depending on context.
+;;
+;; The examples in this file show how we can generate programs
+;; whose behavior differs under these three implementations.
 (load "interp-curried-two-directions.scm")
 (load "../mk/test-check.scm")
 
 
-;; Find a program that evaluates to different values under
-;; left-to-right and right-to-left evaluation:
+;; Find a Scheme expression that evaluates to different values
+;; under left-to-right and right-to-left evaluation:
 (test "1"
   (run 1 (expr v1 v2)
     (=/= v1 v2)
@@ -38,7 +57,7 @@
 
 
 
-;; Find a program that evaluates to either (you) or (lamp):
+;; Find a Scheme expression that evaluates to either (you) or (lamp):
 (test "2"
   (run 1 (expr)
     (eval-left-to-righto expr '(you))
@@ -61,7 +80,7 @@
 
 
 
-;; Find a program that evaluates to either (I love you) or (I love lamp):
+;; Find a Scheme expression that evaluates to either (I love you) or (I love lamp):
 (test "3"
   (run 1 (expr)
     (eval-left-to-righto expr '(I love you))
@@ -85,6 +104,7 @@
 ;; and evaluates to (I love lamp) in Vicare and Petite.
 
 
+;; The left-to-right interpreter can also be used to generate quines.
 (test "quines-left-to-right"
   (run 1 (q)
     (eval-left-to-righto q q))
@@ -93,7 +113,12 @@
      (=/= ((_.0 closure)) ((_.0 cons)) ((_.0 quote)) ((_.0 void)))
      (sym _.0))))
 
-;; doesn't seem to come back
+;; Generating quines using the right-to-left interpreter seems too inefficient
+;; to come back in a reasonable time.  This appears to be due to the awkward
+;; way in which the interpreter evaluates '(cons e1 e2)': ideally the
+;; interpreter should evaluate e1 first, which might immediately result in failure,
+;; and pruning of the search tree.  However, the interpreter evaluates e2 first
+;; (at least logically), which may result in cdring down a list an arbitrary distance.
 #|
 (test "quines-right-to-left"
   (run 1 (q)
